78ff8ed3fdf4197a13032388f6ac3cc6ca787989
jbzm <125964590@qq.com>
2018-04-23 15:31:55 +0800

    finish eureka essay

--- eureka-consumer-ribbon/src/main/java/top/jbzm/cloud/demo/controller/DcController.java
+++ eureka-consumer-ribbon/src/main/java/top/jbzm/cloud/demo/controller/DcController.java
@@ -1,10 +1,14 @@
 package top.jbzm.cloud.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 /**
  * @author jbzm
@@ -15,9 +19,22 @@ public class DcController {
     @Autowired
     @LoadBalanced
     private RestTemplate restTemplate;
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @GetMapping("consumer")
     public String dc() {
         return restTemplate.getForObject("http://eureka-client/dc", String.class);
+    }
+
+    @GetMapping("discovery")
+    public Object look() {
+        List<ServiceInstance> instances = discoveryClient.getInstances("eureka-client");
+        instances.forEach(x->{
+            System.out.println(x.getHost());
+            System.out.println(x.getUri());
+            System.out.println(x.getPort());
+        });
+        return discoveryClient.getInstances("eureka-client");
     }
 }
